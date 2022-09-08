c518f00d9e0c94c73a9d50d5d76a32c054ac8b7c
HenryBass <63601449+HenryBass@users.noreply.github.com>
2022-09-08 13:01:59 -0400

    h

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-width = 128
-height = 128
+width = 100
+height = 100
 var c = document.getElementById("canvas");
 var ctx = canvas.getContext('2d', { alpha: false });
 let csize = 512
@@ -8,8 +8,11 @@ var scaley = (csize / height);
 var drawpos = [0, 0]
 var mdown = false
 let damp = 0.95
-var sheat = true
+
+var sheat = false
 var swave = false
+var sburger = false
+var slaplace = true
 
 var pxs = new Array(width);
 for (var i = 0; i < pxs.length; i++) {
@@ -36,33 +39,51 @@ function d2(pxs, x, y) {
       nx = 0
     }
 
-
-    //console.log(nx)
-
     nx = (nx * 0.25) -  pxs[x][y]
     return nx
+}
+
+function d1(pxs, x, y) {
+  let nx = 0
+  
+  if (x < width - 1) {
+    nx += pxs[x + 1][y]
+} if (x > 0) {
+  nx -= pxs[x - 1][y]
+} if (y < height - 1) {
+  nx += pxs[x][y + 1]
+} if ((y > 0)) {
+  nx -= pxs[x][y - 1]
+} else {
+  nx = 0
+}
+
+  nx = (nx * 0.5)
+  
+  return nx
 }
 
 function getCursorPosition (canvas, e) {
   let rect = canvas.getBoundingClientRect()
   let x = e.clientX - rect.left
   let y = e.clientY - rect.top
-  if (x > 0 && y > 0 && x < csize && y < csize) {
+  if (x > 2 * scalex && y > 2 * scalex && x < csize-1 && y < csize-1) {
     drawpos = [Math.floor(x/scalex), Math.floor(y/scaley)]
   }
 }
 
 setInterval(() => {
   requestAnimationFrame(draw);
-}, 1);
+}, 1000 / 30);
 
 
 function draw() {
   let y = 0
   let x = 0
   if (mdown) {
-    pxs[drawpos[0]][drawpos[1]] += 10
+    pxs[drawpos[0]][drawpos[1]] += 15
   }
+
   while (y < height) {
     while (x < width) {
       px = pxs[x][y];
@@ -82,11 +103,24 @@ function draw() {
   while (y < height) {
     while (x < width) {
       if (sheat) {
-        newmap[x][y] += d2(pxs, x, y) * 0.1;
+        newmap[x][y] += d2(pxs, x, y) * 0.2;
       }
       if (swave) {
         ratemap[x][y] += (d2(pxs, x, y) * 0.1);
       }
+      if (sburger) {
+        let v = 1 * ((d2(pxs, x, y) * 0.2) - (pxs[x][y] * 0.1 * d1(pxs, x, y))); 
+        //v *= 0.9
+
+        if (Math.abs(v) !== Infinity && Math.abs(v) !== NaN && Math.abs(v) < 1000) {
+          newmap[x][y] += v
+        } else {
+        }
+      }
+      if (slaplace) {
+        newmap[x][y] = d2(pxs, x, y) * 0.94;
+      }
+
       newmap[x][y] += (ratemap[x][y] * 1)
       ratemap[x][y] *= damp
       x++;
